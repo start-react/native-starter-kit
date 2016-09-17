bdf86418780b9f62804684d1c420eff512f97b7d
Rowinson Gallego <rwn.gallego@gmail.com>
2016-09-17 12:34:48 -0500

    redux-immutable-state-invariant was added in order to avoid further state mutations

--- js/configureStore.js
+++ js/configureStore.js
@@ -4,14 +4,20 @@
 import { AsyncStorage } from 'react-native'
 import { createStore, applyMiddleware, compose } from 'redux'
 import devTools from 'remote-redux-devtools'
+import immutableStateInvariantMiddleware from 'redux-immutable-state-invariant'
 import { persistStore } from 'redux-persist'
 import thunk from 'redux-thunk'
 import reducer from './reducers'
 import promise from './promise';
 
 export default function configureStore(onCompletion:()=>void):any {
+	// production vs development middlewares
+	const middleware = process.env.NODE_ENV !== 'production' ?
+	[immutableStateInvariantMiddleware(), thunk, promise] :
+	[thunk, promise];
+
 	const enhancer = compose(
-		applyMiddleware(thunk, promise),
+		applyMiddleware(...middleware),
 		devTools({
 	     	name: 'NativeStarterKit', realtime: true
 	    }),
