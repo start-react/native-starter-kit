7043f08e28c78c0d0c430d726f26be9f1a5c8283
Jasbir <jasbir@sahusoft.com>
2017-06-09 20:14:24 +0530

    Redux-Form

--- js/components/login/index.js
+++ js/components/login/index.js
@@ -1,34 +1,93 @@
+import React, { Component } from "react";
+import { Image } from "react-native";
+import { connect } from "react-redux";
+import {
+  Container,
+  Content,
+  Item,
+  Input,
+  Button,
+  Icon,
+  View,
+  Text
+} from "native-base";
+import { Actions } from "react-native-router-flux";
+import { Field, reduxForm } from "redux-form";
+import { setUser } from "../../actions/user";
+import styles from "./styles";
 
-import React, { Component } from 'react';
-import { Image } from 'react-native';
-import { connect } from 'react-redux';
-import { Container, Content, Item, Input, Button, Icon, View, Text } from 'native-base';
-import { Actions } from 'react-native-router-flux';
+const background = require("../../../images/shadow.png");
 
-import { setUser } from '../../actions/user';
-import styles from './styles';
-
-
-const background = require('../../../images/shadow.png');
+const validate = values => {
+  console.log("validate", values);
+  const error = {};
+  error.email = "";
+  error.password = "";
+  var ema = values.email;
+  var pw = values.password;
+  if (values.email === undefined) {
+    ema = "";
+  }
+  if (values.password === undefined) {
+    pw = "";
+  }
+  if (ema.length < 8 && ema !== "") {
+    error.email = "too short";
+  }
+  if (!ema.includes("@") && ema !== "") {
+    error.email = "@ not included";
+  }
+  if (pw.length > 12) {
+    error.password = "max 11 characters";
+  }
+  if (pw.length < 5 && pw.length > 0) {
+    error.password = "Weak";
+  }
+  return error;
+};
 
 class Login extends Component {
-
   static propTypes = {
-    setUser: React.PropTypes.func,
-  }
-
+    setUser: React.PropTypes.func
+  };
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
+      name: ""
     };
+    this.renderInput = this.renderInput.bind(this);
   }
 
   setUser(name) {
     this.props.setUser(name);
   }
-
-
+  renderInput({
+    input,
+    label,
+    type,
+    meta: { touched, error, warning },
+    inputProps
+  }) {
+    var hasError = false;
+    if (error !== undefined) {
+      hasError = true;
+    }
+    return (
+      <Item error={hasError}>
+        <Icon active name={input.name === "email" ? "person" : "unlock"} />
+        <Input
+          placeholder={input.name === "email" ? "EMAIL" : "PASSWORD"}
+          {...input}
+        />
+        {hasError
+          ? <Item style={{ borderColor: "transparent" }}>
+              <Icon active style={{ color: "red", marginTop: 5 }} name="bug" />
+              <Text style={{ fontSize: 15, color: "red" }}>{error}</Text>
+            </Item>
+          : <Text />}
+      </Item>
+    );
+  }
   render() {
     return (
       <Container>
@@ -36,18 +95,12 @@ class Login extends Component {
           <Content>
             <Image source={background} style={styles.shadow}>
               <View style={styles.bg}>
-                <Item style={styles.input}>
-                  <Icon active name="person" />
-                  <Input placeholder="EMAIL" onChangeText={name => this.setState({ name })} />
-                </Item>
-                <Item style={styles.input}>
-                  <Icon name="unlock" />
-                  <Input
-                    placeholder="PASSWORD"
-                    secureTextEntry
-                  />
-                </Item>
-                <Button style={styles.btn} onPress={() => Actions.home()}>
+                <Field name="email" component={this.renderInput} />
+                <Field name="password" component={this.renderInput} />
+                <Button
+                  style={styles.btn}
+                  onPress={() => this.props.navigation.navigate("Home")}
+                >
                   <Text>Login</Text>
                 </Button>
               </View>
@@ -58,12 +111,18 @@ class Login extends Component {
     );
   }
 }
-
-function bindActions(dispatch) {
-  return {
-    setUser: name => dispatch(setUser(name)),
-  };
-}
-
-
-export default connect(null, bindActions)(Login);
+const LoginSwag = reduxForm(
+  {
+    form: "test",
+    validate
+  },
+  function bindActions(dispatch) {
+    return {
+      setUser: name => dispatch(setUser(name))
+    };
+  }
+)(Login);
+LoginSwag.navigationOptions = {
+  header: null
+};
+export default LoginSwag;
--- js/reducers/index.js
+++ js/reducers/index.js
@@ -1,14 +1,12 @@
-
-import { combineReducers } from 'redux';
-
-import drawer from './drawer';
-import user from './user';
-import list from './list';
+import { combineReducers } from "redux";
+import { reducer as formReducer } from "redux-form";
+import drawer from "./drawer";
+import user from "./user";
+import list from "./list";
 
 export default combineReducers({
-
+  form: formReducer,
   drawer,
   user,
-  list,
-
+  list
 });
